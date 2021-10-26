cf19b9baa67f06bc7d34734d539abda632d4360d
bartek <bartosz.natora@gmail.com>
2021-10-26 02:07:40 +0200

    ALERT1_rev.11

---
+++ src/test/java/com/alert/app/backend/service/AirQualityIndexServiceTestSuite.java
@@ -0,0 +1,16 @@
+package com.alert.app.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirQualityIndexServiceTestSuite {
+
+    @Test
+    void shouldGetAll() {
+    }
+
+    @Test
+    void shouldGetByStationId() {
+    }
+}
